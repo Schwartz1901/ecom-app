45e2ebeb241793b47480927e0afb2a0a297e654f
Dinh Thao <=>
2025-06-30 10:53:53 +0000

    init footer

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../shared/navbar/navbar.component';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "../shared/footer/footer.component";
 
 @Component({
   selector: 'app-layout',
-  imports: [NavbarComponent, RouterOutlet],
+  imports: [NavbarComponent, RouterOutlet, FooterComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
