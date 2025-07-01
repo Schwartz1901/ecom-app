f720072b865daecaae35cd651be72cbf6070793d
Dinh Thao <=>
2025-07-01 04:15:49 +0000

    Login page

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { inject } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-
+  private router = inject(Router);
+  login() {
+    this.router.navigate(['home']);
+  }
 }
