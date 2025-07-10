474ed518dd037daab4e4822e46eebfd022ec60fb
Dinh Thao <=>
2025-07-10 04:43:58 +0000

    product controller, interface and service, async implement, testing with postman

--- src/app/documents/week3/week3.component.ts
+++ src/app/documents/week3/week3.component.ts
@@ -87,6 +87,96 @@ public ProductController(IProductService service) {
 `;
   productControllerBadPractice=`// Bad practice
 public ProductController(ProductService service) { }
-`
+`;
+
+  iProductServiceCsHtmlCode=`
+using DocumentAPI.Models;
+
+namespace DocumentAPI.Interfaces
+{
+    public interface IProductService
+    {
+        Task<List<ProductEntity>> GetAllAsync();
+        Task<ProductEntity?> GetByIdAsync(int id);
+        Task AddAsync(ProductEntity product);
+        Task<bool> DeleteAsync(int id);
+    }
+}
+`;
+  productServiceCsHtmlCode=`using DocumentAPI.Interfaces;
+using DocumentAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DocumentAPI.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly DocumentDbContext _context;
+
+        public ProductService(DocumentDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<ProductEntity>> GetAllAsync()
+        {
+            return await _context.ProductEntities.ToListAsync();
+        }
+
+        public async Task<ProductEntity?> GetByIdAsync(int id)
+        {
+            return await _context.ProductEntities.FindAsync(id);
+        }
+
+        public async Task AddAsync(ProductEntity product)
+        {
+            _context.ProductEntities.Add(product);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var product = await _context.ProductEntities.FindAsync(id);
+            if (product == null) return false;
+
+            _context.ProductEntities.Remove(product);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
+`;
+  productControllerCsHtml=`using DocumentAPI.Interfaces;
+using DocumentAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DocumentAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        [HttpGet]
+        // GET api/Product/
+        public async Task<IActionResult> GetAll()
+        {
+            var products = await _productService.GetAllAsync();
+            return Ok(products);
+        }
+        [HttpPost]
+        // POST api/Product 
+        public async Task<IActionResult> Create(ProductEntity product)
+        {
+            await _productService.AddAsync(product);
+            return CreatedAtAction(nameof(GetAll), new { id = product.Id }, product);
+        }
+    }
+}
+`;
 }
 
