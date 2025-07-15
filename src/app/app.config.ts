b434a182fa95c445ae32a49dc1705fd2e7fcc50c
Dinh Thao <=>
2025-07-15 03:45:08 +0000

    auth-interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,12 +2,21 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
+import { AuthInterceptor } from './shared/services/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
     provideHttpClient(),
+    
   ]
 };
+
+export const HttpInterceptorProviders = [
+  {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}
+
+]
+  
+
